d553bd27b91a7f7eb87971cff1f915adbeb201e1
Sara Lucia <saludipu71@gmail.com>
2025-09-12 23:43:55 -0500

    preauthorize

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,5 @@
 export * from './error.handler';
 export * from './logger.middleware';
 export * from './handle.validations.error'
+export * from './auth.middleware';
+export * from './preAuthorize.middleware';
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import { userController } from "../controllers/user.controller";
 import { userValidations } from "../validation";
+import { UserRole } from "../interfaces";
+import { authMiddleware, checkRole } from "../middlewares";
 
 export const router = express.Router();
 
-router.get("/", userController.getAll);
-router.get("/:id", userValidations.id, userController.getOne);
-router.put("/:id", userValidations.id, userValidations.update, userController.update);
+router.get("/", authMiddleware,userController.getAll);
+router.get("/:id", authMiddleware,userValidations.id, userController.getOne);
 router.post("/", userValidations.create, userController.create);
-router.delete("/:id", userValidations.id, userController.delete);
+router.put("/:id", authMiddleware, checkRole(UserRole.ADMIN),userValidations.id, userValidations.update, userController.update);
+router.delete("/:id", authMiddleware, checkRole(UserRole.ADMIN),userValidations.id, userController.delete);
