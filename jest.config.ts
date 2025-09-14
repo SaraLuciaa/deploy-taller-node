26a518f6bf460b4d023ba68e1f6836983c148a5d
Sara Lucia <saludipu71@gmail.com>
2025-09-14 10:59:40 -0500

    init jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["./src/tests"],
+    transform: {
+        "^.+\\.ts?$": "ts-jest"
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts$",
+    moduleFileExtensions: ["ts", "js", "json", "node"],
+};
+
+export default config;
